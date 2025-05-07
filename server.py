d54b7a4a7575821ea1969c02b2b99a5f90466cd6
L050201 <1210843197@qq.com>
2025-05-07 22:50:38 +0800

    12
    Prints statistics for the tuple space, including the number of tuples, the average tuple size, the average key size, the average size, as well as the number of clients, the total number of operations, the number of executions of different operations, and the number of errors

--- server.py
+++ server.py
@@ -39,3 +39,18 @@ class TupleSpace:
         self.get_count += 1
         return value
     #Removes the key-value pair with the specified key from the tuple space and returns its value
+
+    def print_summary(self):
+        tuple_count = len(self.tuples)
+        if tuple_count == 0:
+            avg_tuple_size = 0
+            avg_key_size = 0
+            avg_value_size = 0
+        else:
+            total_tuple_size = sum(len(key) + len(value) for key, value in self.tuples.items())
+            total_key_size = sum(len(key) for key in self.tuples.keys())
+            total_value_size = sum(len(value) for value in self.tuples.values())
+            avg_tuple_size = total_tuple_size / tuple_count
+            avg_key_size = total_key_size / tuple_count
+            avg_value_size = total_value_size / tuple_count
+            #Prints statistics for the tuple space, including the number of tuples, the average tuple size, the average key size, the average size, as well as the number of clients, the total number of operations, the number of executions of different operations, and the number of errors
