36e8bf3de1ab20317230bed35fb1c214503a0cb5
L050201 <1210843197@qq.com>
2025-05-07 22:41:38 +0800

    8
    A tuple space is initialized, and initial values are assigned to properties that record the number of client connections, the total number of operations, the number of different operations, and the number of errors, which can be used to later count and monitor the operations of the tuple space

--- server.py
+++ server.py
@@ -2,3 +2,14 @@ import socket
 import threading
 import time
 # create a socket object
+
+class TupleSpace:
+    def __init__(self):
+        self.tuples = {}
+        self.client_count = 0
+        self.operation_count = 0
+        self.read_count = 0
+        self.get_count = 0
+        self.put_count = 0
+        self.error_count = 0
+        #A tuple space is initialized, and initial values are assigned to properties that record the number of client connections, the total number of operations, the number of different operations, and the number of errors, which can be used to later count and monitor the operations of the tuple space
