0b0d3047b734263d73d1be968994d863504bc6f9
L050201 <1210843197@qq.com>
2025-05-07 22:44:58 +0800

    9
    Inserts a new key-value pair into the tuple space

--- server.py
+++ server.py
@@ -13,3 +13,12 @@ class TupleSpace:
         self.put_count = 0
         self.error_count = 0
         #A tuple space is initialized, and initial values are assigned to properties that record the number of client connections, the total number of operations, the number of different operations, and the number of errors, which can be used to later count and monitor the operations of the tuple space
+    
+    def put(self, key, value):
+        if key in self.tuples:
+            self.error_count += 1
+            return 1
+        self.tuples[key] = value
+        self.put_count += 1
+        return 0    
+    #Inserts a new key-value pair into the tuple space
